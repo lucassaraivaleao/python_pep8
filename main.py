8f11f1e4fb6c303cd4bbc0fdbcaaa6f066b38eed
Lucas Leão <saraivaleaolucas@gmail.com>
2023-01-05 17:18:30 -0300

    descobrindo a pep8 e type hints

---
+++ main.py
@@ -0,0 +1,19 @@
+# from fila_normal import FilaNormal
+
+# fila_teste = FilaNormal()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# print(fila_teste.chama_cliente(5))
+
+from fila_prioritaria import FilaPrioritaria
+
+fila_teste_2 = FilaPrioritaria()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+
+print(fila_teste_2.chama_cliente(10))
+print(fila_teste_2.chama_cliente(13))
+print(fila_teste_2.estatistica('10/01/1995', 198, 'detail'))
